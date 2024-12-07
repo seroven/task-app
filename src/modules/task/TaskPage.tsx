c04e415760ae591cf6ee26e8088a29d3888a5c42
seroven <sebastianrodriguezventura@gmail.com>
2024-12-07 11:35:44 -0500

    feat: add task form component

--- src/modules/task/TaskPage.tsx
+++ src/modules/task/TaskPage.tsx
@@ -3,9 +3,11 @@ import { TaskInterface } from './interfaces/task.interface';
 import { tasks as data } from './data/tasks';
 import { TaskComponent } from './components/TaskComponent';
 import { Button } from 'primereact/button';
+import { FormTaskComponent } from './components/FormTaskComponent';
 
 export const TaskPage = () => {
   const [tasks, setTasks] = useState<TaskInterface[]>([]);
+  const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
     setTasks(data);
@@ -19,17 +21,22 @@ export const TaskPage = () => {
   };
 
   return (
-    <section className="max-w-4xl m-auto">
-      <div className="flex justify-between items-center">
-        <span className="text-4xl font-bold">Tasks</span>
-        <Button icon="pi pi-plus" className="w-8 h-8" />
-      </div>
-      <hr className="border-gray-400 border my-2" />
-      <div className="flex flex-col gap-4">
-        {tasks.map((t, key) => {
-          return <TaskComponent key={key} task={t} onDoneTask={onDoneTask} />;
-        })}
-      </div>
-    </section>
+    <>
+      <section className="max-w-4xl m-auto">
+        <div className="flex justify-between items-center">
+          <span className="text-4xl font-bold">Tasks</span>
+          <Button icon="pi pi-plus" className="w-8 h-8" onClick={() => setShowForm(true)} />
+        </div>
+        <hr className="border-gray-400 border my-2" />
+        <div className="flex flex-col gap-4">
+          {tasks.map((t, key) => {
+            return (
+              <TaskComponent key={key} task={t} onDoneTask={onDoneTask} onShowForm={setShowForm} />
+            );
+          })}
+        </div>
+      </section>
+      <FormTaskComponent visible={showForm} onHide={setShowForm} />
+    </>
   );
 };
--- src/modules/task/components/TaskComponent.tsx
+++ src/modules/task/components/TaskComponent.tsx
@@ -1,29 +1,33 @@
 import { TaskInterface } from '../interfaces/task.interface';
 import { Checkbox } from 'primereact/checkbox';
 import { StarRateComponent } from './StarRateComponent';
+import { useState } from 'react';
+import { FormTaskComponent } from './FormTaskComponent';
 
 interface TaskComponentProps {
   task: TaskInterface;
   onDoneTask: Function;
+	onShowForm: Function
 }
 
-export const TaskComponent = ({ task, onDoneTask }: TaskComponentProps) => {
+export const TaskComponent = ({ task, onDoneTask, onShowForm }: TaskComponentProps) => {
   return (
-    <div className="flex items-stretch gap-4 rounded-lg border p-4 border-gray-700 hover:border-blue-500 transition-all hover:cursor-pointer">
-      <div className="flex-1">
-        <span
-          className={`font-semibold text-blue-500 ${
-            task.done && 'line-through'
-          }`}
-        >
-          {task.summary}
-        </span>
-        <p>{task.description}</p>
+    <>
+      <div
+        className="flex items-stretch gap-4 rounded-lg border p-4 border-gray-700 hover:border-blue-500 transition-all hover:cursor-pointer"
+        onDoubleClick={() => onShowForm(true)}
+      >
+        <div className="flex-1">
+          <span className={`font-semibold text-blue-500 ${task.done && 'line-through'}`}>
+            {task.summary}
+          </span>
+          <p className={task.done ? 'line-through' : ''}>{task.description}</p>
+        </div>
+        <div className="flex gap-2 items-center">
+          <StarRateComponent rate={task.rate} readonly={true} />
+          <Checkbox checked={task.done} onClick={() => onDoneTask(task)} />
+        </div>
       </div>
-      <div className="flex gap-2 items-center">
-        <StarRateComponent rate={task.rate} readonly={true} />
-        <Checkbox checked={task.done} onClick={()=> onDoneTask(task)} />
-      </div>
-    </div>
+    </>
   );
 };
